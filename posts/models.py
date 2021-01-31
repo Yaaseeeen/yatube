6224beae81db39a1cea1941bc58237131a91280d
Mukhtar R <M1@m.m>
2021-01-31 14:27:54 +0600

    users registration template

--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,10 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     group = models.ForeignKey('Group', on_delete=models.SET_NULL, related_name='posts', blank=True, null=True)
 
+    def __str__(self):
+        # выводим текст поста
+        return self.text
+
     class Meta:
         ordering = ['-pub_date']
 
